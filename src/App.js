a89f542e89e2b431234e797b8d6961450569de99
Job <jobjohnson5@gmail.com>
2019-01-10 23:10:06 -0500

    update app,form,index

--- src/App.js
+++ src/App.js
@@ -1,28 +1,139 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import gql from "graphql-tag";
+import { graphql, compose } from "react-apollo";
+import Paper from '@material-ui/core/Paper';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+
+import Form from './Form';
+
+const ShoppinglistsQuery = gql`query{
+  shoppinglists{
+    id
+    text
+    complete
+  }
+}`;
+
+const UpdateShoppinglistMutation = gql`
+  mutation($id: ID!, $complete: Boolean!){
+    updateShoppinglist(id: $id, complete: $complete)
+}`;
+const RemoveShoppinglistMutation = gql`
+  mutation($id: ID!){
+    removeShoppinglist(id: $id)
+}`;
+
+const CreateShoppinglistMutation = gql`
+  mutation($text: String!) {
+    createShoppinglist(text: $text) {
+      id
+      text
+      complete
+    }
+  }`;
 
 class App extends Component {
+
+  updateShoppinglist = async shoppinglist  => {
+    //update
+    await this.props.updateShoppinglist({
+        variables: {
+          id: shoppinglist.id,
+          complete: !shoppinglist.complete
+        },
+    update: (store, { data: { updateShoppinglist} }) => {
+      const data = store.readQuery({ query: ShoppinglistsQuery });
+      data.shoppinglists = data.shoppinglists.map(
+        x =>
+          x.id === shoppinglist.id
+            ? {
+              ...shoppinglist,
+              complete: !shoppinglist.complete
+              }
+              : x
+      );
+      store.writeQuery({ query: ShoppinglistsQuery, data });
+    }
+      });
+  };
+
+  removeShoppinglist = async shoppinglist => {
+    //remove 
+    await this.props.removeShoppinglist({
+      variables: {
+        id: shoppinglist.id
+      },
+  update: (store, { data: { removeShoppinglist} }) => {
+    const data = store.readQuery({ query: ShoppinglistsQuery });
+    data.shoppinglists = data.shoppinglists.filter(
+      x => x.id !== shoppinglist.id
+    );
+    store.writeQuery({ query: ShoppinglistsQuery, data });
+  }
+    });
+};
+
+createShoppinglist = async text => {
+  //create 
+  await this.props.createShoppinglist({
+    variables: {
+      text
+    },
+update: (store, { data: { createShoppinglist } }) => {
+  const data = store.readQuery({ query: ShoppinglistsQuery });
+  data.shoppinglists.unshift(
+    createShoppinglist
+  );
+  store.writeQuery({ query: ShoppinglistsQuery, data });
+}
+  });
+};
+
   render() {
+    const {
+      data: { loading, shoppinglists }
+    } = this.props;
+    if (loading) {
+      return null;
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      //
+        <Paper elevation={1}>
+        <Form submit={this.createShoppinglist} />
+        <List>
+        {shoppinglists.map(shoppinglist => (
+          <ListItem key={`${shoppinglist.id}-shoppinglist-item`} role={undefined} dense button onClick={() => this.updateShoppinglist(shoppinglist)}>
+            <Checkbox
+              checked={shoppinglist.complete}
+              tabIndex={-1}
+              disableRipple
+            />
+            <ListItemText primary={shoppinglist.text} />
+            <ListItemSecondaryAction>
+              <IconButton onClick={() => this.removeShoppinglist(shoppinglist)}>
+                <CloseIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        ))}
+      </List>
+
+        </Paper>
+      //
     );
   }
 }
 
-export default App;
+export default compose(
+graphql(CreateShoppinglistMutation, { name: "createShoppinglist"}),
+graphql(RemoveShoppinglistMutation, { name: "removeShoppinglist"}),
+graphql(UpdateShoppinglistMutation, { name: "updateShoppinglist" }), 
+graphql(ShoppinglistsQuery)
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from "apollo-boost";
+import {ApolloProvider} from 'react-apollo';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+//import ApolloClient from 'apollo-client';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const client = new ApolloClient({
+    //uri: "http://localhost:4000"
+    //uri: "http://192.168.1.159:4000/"
+    uri: "https://jj-shoppinglist-server.herokuapp.com/"
+});
+
+
+ReactDOM.render(
+<ApolloProvider client={client}>
+<App />
+</ApolloProvider>, 
+document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
